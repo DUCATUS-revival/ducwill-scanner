4bca1a0a2422437a1d8731b2dec41c56e4c6ee85
Mikhail Rozdin <mrozdin@yandex.ru>
2020-04-23 17:50:58 +0300

    erc20 payment monitor

--- mywish-model/src/main/java/io/lastwill/eventscan/model/NetworkProviderType.java
+++ mywish-model/src/main/java/io/lastwill/eventscan/model/NetworkProviderType.java
@@ -4,4 +4,5 @@ public enum  NetworkProviderType {
     DUCX,
     BTC,
     DUC,
+    ETH,
 }
--- mywish-model/src/main/java/io/lastwill/eventscan/model/NetworkType.java
+++ mywish-model/src/main/java/io/lastwill/eventscan/model/NetworkType.java
@@ -5,6 +5,8 @@ import lombok.Getter;
 @Getter
 public enum NetworkType {
     DUCATUSX_MAINNET(NetworkProviderType.DUCX),
+    ETHEREUM_MAINNET(NetworkProviderType.ETH),
+    ETHEREUM_TESTNET(NetworkProviderType.ETH),
     DUCATUSX_TESTNET(NetworkProviderType.DUCX),
     DUCATUS_MAINNET(NetworkProviderType.DUC),
     DUCATUS_TESTNET(NetworkProviderType.DUC),
@@ -13,6 +15,8 @@ public enum NetworkType {
     public final static String DUC_TESTNET_VALUE = "DUCATUS_TESTNET";
     public final static String DUCX_MAINNET_VALUE = "DUCATUSX_MAINNET";
     public final static String DUCX_TESTNET_VALUE = "DUCATUSX_TESTNET";
+    public final static String ETH_MAINNET_VALUE = "ETHEREUM_MAINNET";
+    public final static String ETH_TESTNET_VALUE = "ETHEREUM_TESTNET";
 
     private final NetworkProviderType networkProviderType;
 
