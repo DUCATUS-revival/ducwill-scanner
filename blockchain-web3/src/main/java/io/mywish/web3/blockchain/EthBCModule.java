691840b945ed25fdb86c401868575e40705d71e3
Mikhail Rozdin <mrozdin@yandex.ru>
2020-04-23 19:43:23 +0300

    ethereum scanner

--- blockchain-web3/src/main/java/io/mywish/web3/blockchain/EthBCModule.java
+++ blockchain-web3/src/main/java/io/mywish/web3/blockchain/EthBCModule.java
@@ -20,57 +20,58 @@ import java.net.URI;
 
 @Configuration
 @ComponentScan
-public class Web3BCModule {
-    @ConditionalOnProperty(name = "io.lastwill.eventscan.ducatusx.mainnet")
-    @Bean(name = NetworkType.DUCX_MAINNET_VALUE)
+public class EthBCModule {
+    @ConditionalOnProperty(name = "io.lastwill.eventscan.web3-url.ethereum")
+    @Bean(name = NetworkType.ETH_MAINNET_VALUE)
     public Web3Network ethNetMain(
-            @Value("${io.lastwill.eventscan.ducatusx.mainnet}") URI web3Url,
+            @Value("${io.lastwill.eventscan.web3-url.mainnet}") URI web3Url,
             @Value("${etherscanner.polling-interval-ms:5000}") Long pollingInterval,
-            @Value("${etherscanner.pending-transactions-threshold}") int pendingThreshold) throws ConnectException {
+            @Value("${etherscanner.eth.pending-transactions-threshold}") int pendingThreshold) throws ConnectException {
         return new Web3Network(
-                NetworkType.DUCATUSX_MAINNET,
+                NetworkType.ETHEREUM_MAINNET,
                 new WebSocketClient(web3Url),
                 pollingInterval,
                 pendingThreshold);
     }
 
-    @ConditionalOnProperty(name = "io.lastwill.eventscan.ducatusx.testnet")
-    @Bean(name = NetworkType.DUCX_TESTNET_VALUE)
+    @ConditionalOnProperty(name = "io.lastwill.eventscan.web3-url.testnet")
+    @Bean(name = NetworkType.ETH_TESTNET_VALUE)
     public Web3Network ethNetRopsten(
-            @Value("${io.lastwill.eventscan.ducatusx.testnet}") URI web3Url,
+            @Value("${io.lastwill.eventscan.web3-url.testnet}") URI web3Url,
             @Value("${etherscanner.polling-interval-ms:5000}") Long pollingInterval,
-            @Value("${etherscanner.pending-transactions-threshold}") int pendingThreshold) throws ConnectException {
+            @Value("${etherscanner.eth.pending-transactions-threshold}") int pendingThreshold) throws ConnectException {
         return new Web3Network(
-                NetworkType.DUCATUSX_TESTNET,
+                NetworkType.ETHEREUM_TESTNET,
                 new WebSocketClient(web3Url),
                 pollingInterval,
                 pendingThreshold);
     }
 
     @Configuration
-    public class EthDbPersisterConfiguration {
+    public static class EthDbPersisterConfiguration {
         @Bean
         public LastBlockPersister ethMainnetLastBlockPersister(
                 LastBlockRepository lastBlockRepository
         ) {
-            return new LastBlockDbPersister(NetworkType.DUCATUSX_MAINNET, lastBlockRepository, null);
+            return new LastBlockDbPersister(NetworkType.ETHEREUM_MAINNET, lastBlockRepository, null);
         }
 
         @Bean
         public LastBlockPersister ethRopstenLastBlockPersister(
                 LastBlockRepository lastBlockRepository
         ) {
-            return new LastBlockDbPersister(NetworkType.DUCATUSX_TESTNET, lastBlockRepository, null);
+            return new LastBlockDbPersister(NetworkType.ETHEREUM_TESTNET, lastBlockRepository, null);
         }
     }
 
-    @ConditionalOnBean(name = NetworkType.DUCX_MAINNET_VALUE)
+    @ConditionalOnBean(name = NetworkType.ETH_MAINNET_VALUE)
     @Bean
-    public Web3Scanner ducxScannerMain(
-            final @Qualifier(NetworkType.DUCX_MAINNET_VALUE) Web3Network network,
+    public Web3Scanner ethScannerMain(
+            final @Qualifier(NetworkType.ETH_MAINNET_VALUE) Web3Network network,
             final @Qualifier("ethMainnetLastBlockPersister") LastBlockPersister lastBlockPersister,
             final @Value("${etherscanner.polling-interval-ms:5000}") Long pollingInterval,
-            final @Value("${etherscanner.commit-chain-length:5}") Integer commitmentChainLength
+            final @Value("${etherscanner.commit-chain-length:5}") Integer commitmentChainLength,
+            final @Value("${etherscanner.eth.accelerator}") Integer accelerator
     ) {
         return new Web3Scanner(
                 network,
@@ -80,13 +81,14 @@ public class Web3BCModule {
         );
     }
 
-    @ConditionalOnBean(name = NetworkType.DUCX_TESTNET_VALUE)
+    @ConditionalOnBean(name = NetworkType.ETH_TESTNET_VALUE)
     @Bean
-    public Web3Scanner ducxScannerTestnet(
-            final @Qualifier(NetworkType.DUCX_TESTNET_VALUE) Web3Network network,
+    public Web3Scanner ethScannerRopsten(
+            final @Qualifier(NetworkType.ETH_TESTNET_VALUE) Web3Network network,
             final @Qualifier("ethRopstenLastBlockPersister") LastBlockPersister lastBlockPersister,
             final @Value("${etherscanner.polling-interval-ms:5000}") Long pollingInterval,
-            final @Value("${etherscanner.commit-chain-length:5}") Integer commitmentChainLength
+            final @Value("${etherscanner.commit-chain-length:5}") Integer commitmentChainLength,
+            final @Value("${etherscanner.eth.accelerator}") Integer accelerator
     ) {
         return new Web3Scanner(
                 network,
