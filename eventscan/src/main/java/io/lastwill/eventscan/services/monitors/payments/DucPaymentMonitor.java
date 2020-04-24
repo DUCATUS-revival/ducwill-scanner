db3c87f3567c8f1978919cfeea74ab1433c8d4e0
Mikhail Rozdin <mrozdin@yandex.ru>
2020-04-24 18:04:30 +0300

    disable duc payment monitor

--- eventscan/src/main/java/io/lastwill/eventscan/services/monitors/payments/DucPaymentMonitor.java
+++ eventscan/src/main/java/io/lastwill/eventscan/services/monitors/payments/DucPaymentMonitor.java
@@ -17,7 +17,7 @@ import java.util.List;
 import java.util.Set;
 
 @Slf4j
-@Component
+//@Component
 public class DucPaymentMonitor {
     @Autowired
     private UserSiteBalanceRepository userSiteBalanceRepository;
